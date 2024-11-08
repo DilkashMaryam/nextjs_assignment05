fd0171606f588f89fa7c14a7372799b8434f9e60
DilkashMaryam <dilkashmaryam9@gmail.com>
2024-11-08 09:58:30 +0500

    First commit

---
+++ app/fonts/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = ()=> {
+    return <div>About PAge</div>;
+}
+
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,32 @@
-import Image from "next/image";
+import Header from "@/components/Header";
+import Image from "next/image"
+import {Rye, Libre_Bodoni} from "next/font/google"
+
+const rye = Rye({ subsets: ['latin'], weight: '400' });
+const libreBodoni = Libre_Bodoni({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen flex felx-col">
+       <div className="w-full max-w-7xl bg-[#FFFFFF] flex flex-col">
+        <Header></Header>
+        <div className="w-[50%] ml-[50px] bg-[#FFFFFF] flex flex-col md:flex-row items-center justify-between gap-10 p-8 md:p-16">
+        <div className="w-1/2 ml-[200px] flex-1 flex justify-center">
+        <div className="flex-1">
+          <h1 className="w-[472.97] mt-[50px] ml-[50px] font-bold text-4xl mb-4 font-[LibreBodoni-Regular]">IMPECCABLE CRAFTSMANSHIP AND FINESSE</h1>
+          <p className="font-serif font-medium text-2xl mb-6 text-[#787054] w-[600] mt-[50px] ml-[50px]">
+            An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
+            <button className="w-[247.67] h-[49.2] mt-[20p] ml-[50px] rounded-[8.6] p-[8.6] ga-[8.6] bg-[#A29875]">
+              <span className="w-[153] h-[32] font-[LibreBodoni-Regular] text-[25.8] text-[#FFFFFF] pt-[4.5] pl-[35]">EXPLORE NOW</span></button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="w-[421.38] h-[573.59] relative mt-[10px] ml-[400] rounded-tl-[128.99] rounded-br-[128.99] border-[0.86px] border-solid border-transparent flex">
+        <Image src="/images/picture.jpeg" alt="picture" width={421.38} height={573.59}
+        className="rounded-tl-[128.99] rounded-br-[128.99] object-cover absolute"/>
+            <div className="ml-[10px] mr-[10px] mt-[7px] w-[500px] h-[550px] border-2 rounded-tl-[128.99] rounded-br-[128.99] border-[#FFFFFF] z-10"></div>
+        </div>
+        </div>
+        </div> 
     </div>
   );
 }
